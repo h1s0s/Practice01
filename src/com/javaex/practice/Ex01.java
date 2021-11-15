a360cad6adcbc0f8547529c12501f31410fe19ec
27_짝수_한상선 <91957568+h1s0s@users.noreply.github.com>
2021-11-15 15:37:43 +0900

    Update Ex01.java

--- src/com/javaex/practice/Ex01.java
+++ src/com/javaex/practice/Ex01.java
@@ -3,11 +3,11 @@ package com.javaex.practice;
 public class Ex01 {
 	public static void main(String[] args) {
 		
-		int true01;//예약어 X
-		int _of_workers;//$,_외의 특수문자 사용 불가.
-		int countOfLettersInString;//?
-		int firstLevel1;//숫자로 시작X
-		int person;//$,_외의 특수문자 사용 불가.
-		int _person;//?
+		int true01;//X int true; -> int true01; (예약어 사용 불가)
+		int _of_workers;//X #_of_workers; -> _of_workers; ($,_외의 특수문자 사용 불가.)
+		int countOfLettersInString;//O 오류 없음
+		int firstLevel1;//X 1stLevel1 -> firstLevel1 (숫자로 시작X)
+		int person;//X person#; -> person; $,_외의 특수문자 사용 불가.
+		int _person;//O 오류 없음
 	}
 }
